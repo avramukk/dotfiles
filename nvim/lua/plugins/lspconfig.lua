ecd358f348024f6ee22742c91176c2368f96d3ac
Nikolai Avramuk <avramuk89@gmail.com>
2024-04-01 01:35:51 +0100

    added lsp servers

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -6,16 +6,17 @@ return {
       ---@type lspconfig.options
       servers = {
         -- pyright will be automatically installed with mason and loaded with lspconfig
-        -- pyright = {},
+        pyright = {},
         bashls = {},
-        -- gopls = {},
         bicep = {},
         yamlls = {},
         marksman = {},
         powershell_es = {},
-        -- azure_pipelines_ls = {},
         terraformls = {},
         helm_ls = {},
+        jsonls = {},
+        html = {},
+        cssls = {},
       },
     },
   },
