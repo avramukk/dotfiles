f7fa2d22d74da30f33cc317ec0a6c9194704abf2
avramukk <avramuk89@gmail.com>
2024-07-02 21:30:35 +0100

    added formatting window for markdown and json

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -10,6 +10,19 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
+
+-- Set textwidth to 80 and automatic line breaks for markdown files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "gitcommit", "markdown", "pandoc" },
+  callback = function()
+    require("cmp").setup({ enabled = false })
+    vim.opt_local.textwidth = 80
+    -- vim.opt_local.formatoptions:append("a")
+    vim.opt_local.colorcolumn = "80"
+  end,
+})
+
+
 -- attemting to disable terraform ls on fixture file
 -- vim.api.nvim_create_autocmd("BufEnter", {
 --   pattern = "*fixture*",
@@ -60,4 +73,14 @@ vim.api.nvim_create_autocmd("filetype", {
   -- group = augroup("wrap_spell"),
   pattern = { "go" },
   command = 'lua require("cmp").setup { enabled = true }',
+})
+
+
+-- Disable the concealing in some file formats
+-- The default conceallevel is 3 in LazyVim
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "json", "jsonc", "markdown" },
+	callback = function()
+		vim.opt.conceallevel = 0
+	end,
 })
